e6d543a2fd9e22e3b458a8116e66394315870ab5
0xKato <106168398+shr1mpii@users.noreply.github.com>
2022-08-22 17:20:57 +0200

    final commit

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -5,13 +5,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-/**
- * @title DEX Template
- * @author stevepham.eth and m00npapi.eth
- * @notice Empty DEX.sol that just outlines what features could be part of the challenge (up to you!)
- * @dev We want to create an automatic market where our contract will hold reserves of both ETH and 🎈 Balloons. These reserves will provide liquidity that allows anyone to swap between the assets.
- * NOTE: functions outlined here are what work with the front end of this branch/repo. Also return variable names that may need to be specified exactly may be referenced (if you are confused, see solutions folder in this repo and/or cross reference with front-end code).
- */
+
 contract DEX {
     /* ========== GLOBAL VARIABLES ========== */
 
@@ -25,22 +19,17 @@ contract DEX {
     /**
      * @notice Emitted when ethToToken() swap transacted
      */
-    event EthToTokenSwap(address recipient, string ethToBallons, uint256 ethAmount, uint256 tokenAmount);
+    event EthToTokenSwap(address recipient, uint256 ethAmount, uint256 tokenAmount);
 
     /**
      * @notice Emitted when tokenToEth() swap transacted
      */
-    event TokenToEthSwap(address recipient, string baloonsToEth, uint256 tokenAmount, uint256 ethAmount);
+    event TokenToEthSwap(address recipient, uint256 tokenAmount, uint256 ethAmount);
 
     /**
      * @notice Emitted when liquidity provided to DEX and mints LPTs.
      */
-    event LiquidityProvided(
-        address recipient,
-        uint256 liquidityMinted,
-        uint256 ethAmount,
-        uint256 tokenAmount
-    );
+    event LiquidityProvided(address recipient, uint256 liquidityMinted, uint256 ethAmount, uint256 tokenAmount);
 
     /**
      * @notice Emitted when liquidity removed from DEX and decreases LPT count within DEX.
@@ -49,7 +38,7 @@ contract DEX {
 
     /* ========== CONSTRUCTOR ========== */
 
-    constructor(address token_addr) public {
+    constructor(address token_addr) {
         token = IERC20(token_addr); //specifies the token address that will hook into the interface and be used through the variable 'token'
     }
 
@@ -73,11 +62,7 @@ contract DEX {
      * @notice returns yOutput, or yDelta for xInput (or xDelta)
      * @dev Follow along with the [original tutorial](https://medium.com/@austin_48503/%EF%B8%8F-minimum-viable-exchange-d84f30bd0c90) Price section for an understanding of the DEX's pricing model and for a price function to add to your contract. You may need to update the Solidity syntax (e.g. use + instead of .add, * instead of .mul, etc). Deploy when you are done.
      */
-    function price(
-        uint256 xInput,
-        uint256 xReserves,
-        uint256 yReserves
-    ) public view returns (uint256 yOutput) {
+    function price(uint256 xInput, uint256 xReserves,uint256 yReserves) public pure returns (uint256 yOutput) {
         uint256 xInputWithFee = xInput.mul(997);
         uint256 numerator = xInputWithFee.mul(yReserves);
         uint256 denominator = (xReserves.mul(1000)).add(xInputWithFee);
@@ -97,14 +82,13 @@ contract DEX {
      * @notice sends Ether to DEX in exchange for $BAL
      */
     function ethToToken() public payable returns (uint256 tokenOutput) {
-        require(msg.value > 0, "Not enough ether to swap for tokens");
+        require(msg.value > 0, "cannot swap 0 ETH");
+        uint256 ethReserve = address(this).balance.sub(msg.value);
+        uint256 token_reserve = token.balanceOf(address(this));
+        tokenOutput = this.price(msg.value, ethReserve, token_reserve);
 
-        uint256 preTxReserves = address(this).balance - msg.value;
-        uint256 tokenReserves = token.balanceOf(address(this));
-        tokenOutput = this.price(msg.value, preTxReserves, tokenReserves);
-
-        require(token.transfer(msg.sender, tokenOutput));
-        emit EthToTokenSwap(msg.sender, "Eth -> Balloons",msg.value,tokenOutput);
+        require(token.transfer(msg.sender, tokenOutput), "ethToToken(): reverted swap.");
+        emit EthToTokenSwap(msg.sender, msg.value, tokenOutput);
         return tokenOutput;
     }
 
@@ -114,9 +98,8 @@ contract DEX {
     function tokenToEth(uint256 tokenInput) public returns (uint256 ethOutput) {
         require(tokenInput > 0, "You need to specify amount of tokens to swap");
 
-        uint256 ethReserves = address(this).balance;
-        uint256 tokenReserves = token.balanceOf(address(this));
-        ethOutput = this.price(tokenInput, tokenReserves, ethReserves);
+        uint256 token_reserve = token.balanceOf(address(this));
+        ethOutput = price(tokenInput, token_reserve, address(this).balance);
 
         require(
             token.transferFrom(msg.sender, address(this), tokenInput),
@@ -124,7 +107,7 @@ contract DEX {
         );
         (bool sent, ) = msg.sender.call{value: ethOutput}("");
         require(sent, "Unable to transfer ETH to you");
-        emit EthToTokenSwap(msg.sender, "Balloons -> ETH", ethOutput, tokenInput);
+        emit TokenToEthSwap(msg.sender, ethOutput, tokenInput);
         return ethOutput;
     }
     /**
